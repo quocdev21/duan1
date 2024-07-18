ee574bd7b35de19c30be9dafececfa158c0df349
Nguyễn Huy Hoàng <hoangnguyeb97@gmail.com>
2024-07-19 04:15:18 +0700

    Update DBConnect.java

--- Quan_Ly_Ban_Ve_May_bay/src/config/DBConnect.java
+++ Quan_Ly_Ban_Ve_May_bay/src/config/DBConnect.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class DBConnect {
     public static final String HOSTNAME = "localhost";
     public static final String PORT = "1433";
-    public static final String DBNAME = "QLGV";
+    public static final String DBNAME = "BANVEMAYBAY";
     public static final String USERNAME = "sa";
-    public static final String PASSWORD = "123456";
+    public static final String PASSWORD = "1";
 
 
     public static Connection getConnection() {
