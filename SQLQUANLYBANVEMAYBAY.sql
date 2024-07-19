6b121a31c4c6edc0c19ee18a3545c1540d5051c0
SimpMahiru <cuhoaianhvlog@gmail.com>
2024-07-19 11:28:52 +0700

    update sql

--- SQLQUANLYBANVEMAYBAY.sql
+++ SQLQUANLYBANVEMAYBAY.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE BANVEMAYBAY
 GO
 USE BANVEMAYBAY
-DROP TABLE VE
+DROP TABLE MAYBAY
 CREATE TABLE MAYBAY(
 	IDMayBay VARCHAR(50) PRIMARY KEY,
 	SoHieuMayBay VARCHAR(50) NOT NULL,
@@ -23,7 +23,7 @@ VALUES('VN-A101','VN - Vietnam Airlines','Vietnam Airlines',250),
 CREATE TABLE CHUYENBAY(
 	IDChuyenBay VARCHAR(50) PRIMARY KEY,
 	IDMayBay VARCHAR(50) FOREIGN KEY REFERENCES MAYBAY(IDMayBay),
-	HangHangKhong nvarchar(225) not null,
+	NgayDi Date not null,
 	ThoiGianDi INT NOT NULL,
 	ThoiGianDen INT NOT NULL,
 	ThoiGianTre INT NOT NULL,
@@ -31,15 +31,15 @@ CREATE TABLE CHUYENBAY(
 )
 
 INSERT INTO CHUYENBAY
-VALUES('VN112','VN-A103','VietNam Airlines',1920,2130,20,'Chuyen bay di HCM'),
-('VJ113','VJ-A102','Vietjet Air',800,950,0,'Chuyen bay di Nha Trang'),
-('VN114','VN-A101','VietNam Airlines',400,530,0,'Chuyen bay di Hue'),
-('VJ115','VJ-A104','Vietjet Air',1120,1240,0,'Chuyen bay di Da Nang'),
-('VN116','VN-A106','VietNam Airlines',120,320,0,'Chuyen bay di Ha Noi'),
-('VJ117','VJ-A105','Vietjet Air',2020,2150,0,'Chuyen bay di Da Nang'),
-('VN118','VN-A107','VietNam Airlines',1520,1650,0,'Chuyen bay di Hue'),
-('VN119','VN-A108','VietNam Airlines',1920,2130,0,'Chuyen bay di HCM'),
-('VJ111','VJ-A109','Vietjet Air',1220,1400,0,'Chuyen bay di Nha Trang')
+VALUES('VN112','VN-A103','2024-6-12',1920,2130,20,'Chuyen bay di HCM'),
+('VJ113','VJ-A102','2024-6-4',800,950,0,'Chuyen bay di Nha Trang'),
+('VN114','VN-A101','2024-5-26',400,530,0,'Chuyen bay di Hue'),
+('VJ115','VJ-A104','2024-5-26',1120,1240,0,'Chuyen bay di Da Nang'),
+('VN116','VN-A106','2024-5-19',120,320,0,'Chuyen bay di Ha Noi'),
+('VJ117','VJ-A105','2024-5-17',2020,2150,0,'Chuyen bay di Da Nang'),
+('VN118','VN-A107','2024-5-17',1520,1650,0,'Chuyen bay di Hue'),
+('VN119','VN-A108','2024-4-22',1920,2130,0,'Chuyen bay di HCM'),
+('VJ111','VJ-A109','2024-4-21',1220,1400,0,'Chuyen bay di Nha Trang')
 
 CREATE TABLE VE(
 	IDVe VARCHAR(50) PRIMARY KEY,
@@ -55,7 +55,7 @@ CREATE TABLE VE(
 INSERT INTO VE
 VALUES('A123-133454','VN112','AFF121','HA NOI','TP HO CHI MINH','D','2024-02-16',4000000),
 ('A123-123454','VJ115','AFF122','HA NOI','DA NANG','D1','2024-02-14',200000),
-('A123-143454','VN116','AFF123','DA NANG','HA NOI','G1','2024-11-19',1500000),
+('A123-143454','VN116','AFF123','DA NANG','HA NOI','G1','2023-11-19',1500000),
 ('A123-153454','VJ111','AFF124','HA NOI','NHA TRANG','D2','2023-10-20',360000),
 ('A123-163454','VJ117','AFF125','HA NOI','DA NANG','G2','2022-04-28',1500000),
 ('A123-173454','VJ113','AFF126','HA NOI','NHA TRANG','D3','2021-04-30',3000000),
